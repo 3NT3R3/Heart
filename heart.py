8bfba730a5e0915512352932339005337946a579
3NT3R3 <100038338+3NT3R3@users.noreply.github.com>
2022-04-28 21:31:38 +0200

    heart.py is main file.

---
+++ heart.py
@@ -0,0 +1,55 @@
+#" Author - 3NT3R3 or "Anon"
+#
+# Github - https://github.com/3NT3R3
+#
+# Target this project - You can see it with your girlfriend.
+#
+# It is my first project on Github.
+#
+#
+#
+#
+#
+# """
+
+import turtle
+from time import sleep
+
+turtle.pensize(5)       # Where items to our heart can change(ep. different color)
+turtle.speed(1)         #
+turtle.color('red')     #
+turtle.begin_fill()     #
+turtle.fillcolor('red') #
+
+
+
+turtle.left(150)
+turtle.forward(180)
+turtle.circle(-90,180)
+turtle.setheading(60)
+turtle.circle(-90,180)
+turtle.forward(180)
+turtle.end_fill()
+turtle.mainloop()
+
+print("I love you <3")
+sleep(1.5)
+print("            ###          ###                          ")
+sleep(1)
+print("          ##   ##########   ##                       ")
+sleep(1)
+print("           ##             ##                        ")
+sleep(1)
+print("             ##         ##                        ")
+sleep(1)
+print("               ##     ##                              ")
+sleep(1)
+print("                  ###                         ")
+sleep(1)
+print("                   #                                      ")
+#" Ok, all is ready but how start ?
+#  You start this ,when heart will paint close turtle window and say her to see down (on "I love you <3")
+#
+#
+#
+# "
